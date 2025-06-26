b66ec85538728f95b28f6a354b28cb1604d2f8de
Theophilus Asamoah Gyapong <jhapson34@gmail.com>
2025-06-26 17:30:50 +0000

    log into twitter, tweet complaint and post

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from time import sleep
 
 from selenium import webdriver
 from selenium.webdriver import ChromeOptions
@@ -11,6 +12,40 @@ options = ChromeOptions.add_experimental_option('detach', True)
 complaint_bot_driver = webdriver.Chrome(options=options)
 
 complaint_bot_driver.get('https://x.com/i/flow/login')
-username = WebDriverWait(complaint_bot_driver, 15).until(
-    expected_conditions.presence_of_element_located((By.NAME, 'text')))
-username.send_keys(os.environ.get('USERNAME'), Keys.ENTER)
+
+# execute if login ui is present
+try:
+    # username
+    username = WebDriverWait(complaint_bot_driver, 15).until(
+        expected_conditions.presence_of_element_located((By.NAME, 'text')))
+    username.send_keys(os.environ.get('USERNAME'), Keys.ENTER)
+    sleep(2)
+
+except Exception as e:
+    print('Already logged in or username invalid...Exception:', e)
+
+else:
+    try:
+        # password
+        password = WebDriverWait(complaint_bot_driver, 7).until(
+            expected_conditions.presence_of_element_located((By.NAME, 'password')))
+        password.send_keys(os.environ.get('PASSWORD'), Keys.ENTER)
+    except Exception as e:
+        print('Incorrect password...Exception:', e)
+
+
+# tweet and post
+tweet_button = WebDriverWait(complaint_bot_driver, 10).until(
+    expected_conditions.element_to_be_clickable((By.CSS_SELECTOR, 'a[aria-label="Post"]')))
+tweet_button.click()
+
+sleep(2)
+tweet = complaint_bot_driver.find_element(
+    By.CSS_SELECTOR, 'div[aria-label="Post text"] .public-DraftStyleDefault-block.public-DraftStyleDefault-ltr')
+tweet.click()
+tweet.send_keys('Slow internet @telecel')
+
+sleep(2)
+post_tweet = complaint_bot_driver.find_element(
+    By.CSS_SELECTOR, 'button[data-testid="tweetButton"]')
+post_tweet.click()
